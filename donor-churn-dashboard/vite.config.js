5ad1af23dcf373712ff36fd87f7c52d43f9c5fca
JiYoon241111 <hhongjiyoonn@gmail.com>
2026-07-29 11:28:01 +0900

    fix: [MLP-4] vite.config.js 버그 수정

--- donor-churn-dashboard/vite.config.js
+++ donor-churn-dashboard/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {       // 추가
+    port: 5173,   //추가
+    strictPort: true,   //추가
+  },
 })
